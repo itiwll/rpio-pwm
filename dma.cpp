a8f27088c961cc2e2cabed9141eb42a140338c18
Eson Wong <itiwll@gmail.com>
2021-08-16 12:31:18 +0800

    suported new pi system

--- dma.cpp
+++ dma.cpp
@@ -515,7 +515,7 @@ void get_model_and_revision(DmaHardware &hw) {
 
   if (strstr(modelstr, "BCM2708"))
     hw.board_model = 1;
-  else if (strstr(modelstr, "BCM2709") || strstr(modelstr, "BCM2835"))
+  else if (strstr(modelstr, "BCM2709") || strstr(modelstr, "BCM2835") || strstr(modelstr, "BCM2711"))
     hw.board_model = 2;
   else
     fatal("rpio-pwm: Cannot parse the hardware name string\n");
